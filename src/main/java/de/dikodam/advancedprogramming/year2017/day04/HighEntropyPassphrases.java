acb3194ef5efc43f7f6e7d5b2dc22e94e24df599
dikodam <adiko@gmx.de>
2020-11-12 10:32:45 +0100

    2017 day 04 Set vorgestellt

---
+++ src/main/java/de/dikodam/advancedprogramming/year2017/day04/HighEntropyPassphrases.java
@@ -0,0 +1,14 @@
+package de.dikodam.advancedprogramming.year2017.day04;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class HighEntropyPassphrases {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+
+        boolean warNochNichtDrin = set.add(1);
+
+    }
+}
