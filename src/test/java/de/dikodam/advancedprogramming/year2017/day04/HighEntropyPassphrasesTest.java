fc96e0edfe4581efef4b36d6fdaa7c43c21ffcf4
dikodam <adiko@gmx.de>
2020-11-12 14:19:31 +0100

    2017 day 04 isPassphrase valid implementiert

--- src/test/java/de/dikodam/advancedprogramming/year2017/day04/HighEntropyPassphrasesTest.java
+++ src/test/java/de/dikodam/advancedprogramming/year2017/day04/HighEntropyPassphrasesTest.java
@@ -2,6 +2,10 @@ package de.dikodam.advancedprogramming.year2017.day04;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +26,18 @@ class HighEntropyPassphrasesTest {
         boolean actualResult = HighEntropyPassphrases.isPassphraseValid("aa bb cc dd aa");
 
         assertThat(actualResult).isFalse();
+    }
+
+    @Test
+    void countValidPassphrases() {
+        // setup
+        List<String> input = asList("aa bb cc dd ee", "aa bb cc dd aa");
+
+        // execute
+        int actualResult = HighEntropyPassphrases.countValidPassphrases(input);
+
+        // verify
+        assertThat(actualResult).isEqualTo(1);
     }
 
 
