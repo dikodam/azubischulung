e4dec812b776afe751cf2cbf2eb072f5897341d1
dikodam <adiko@gmx.de>
2020-12-23 08:44:42 +0100

    2019 day 03 setup of OOM

---
+++ src/main/java/de/dikodam/advancedprogramming/year2019/day03/CrossedWires.java
@@ -0,0 +1,10 @@
+package de.dikodam.advancedprogramming.year2019.day03;
+
+public class CrossedWires {
+
+    // Eine Klasse Wire, mit dem Attribut path, vom Typ "Liste an Punkten"
+    // Eine Klasse Point, die x- und y-Koordinaten kapselt
+    // Eine Klasse Command, die Befehle wie "R221" kapselt, mit den Attributen Direction und stepCount
+    // Ein Enum Direction mit den vier Ausprägungen up, down, left, right
+
+}
