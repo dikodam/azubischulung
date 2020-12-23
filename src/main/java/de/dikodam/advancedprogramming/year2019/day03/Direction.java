fdfcc89ae8ad9dc3fde15509fb31e7346aad4955
dikodam <adiko@gmx.de>
2020-12-23 10:55:20 +0100

    2016 day 01 Command parsing

--- src/main/java/de/dikodam/advancedprogramming/year2019/day03/Direction.java
+++ src/main/java/de/dikodam/advancedprogramming/year2019/day03/Direction.java
@@ -2,4 +2,20 @@ package de.dikodam.advancedprogramming.year2019.day03;
 
 public enum Direction {
     UP, DOWN, LEFT, RIGHT;
+
+    public static Direction from(char directionChar) {
+        if (directionChar == 'L') {
+            return LEFT;
+        }
+        if (directionChar == 'D') {
+            return DOWN;
+        }
+        if (directionChar == 'U') {
+            return UP;
+        }
+        if (directionChar == 'R') {
+            return RIGHT;
+        }
+        return null;
+    }
 }
